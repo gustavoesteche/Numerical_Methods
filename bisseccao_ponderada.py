370fa653641b1a6c3b0238e76481ea3ba5c4d5dc
Gustavo Esteche Araujo <gustavoestechea@gmail.com>
2023-08-08 17:27:04 -0300

    adição do chefe

---
+++ bisseccao_ponderada.py
@@ -0,0 +1,14 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def bisseccao_ponderada_x():
+    pass
+
+def bisseccao_ponderada_y():
+    pass
+
+def f(x):
+    pass
+
+def plot_grafico():
+    pass
